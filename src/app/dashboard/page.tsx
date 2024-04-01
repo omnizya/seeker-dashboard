55a6a19598837c42869f034fefd2e867f9ece32c
EisenUlfr <moughamir@gmail.com>
2024-04-01 02:59:03 +0000

    exp

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,10 +4,20 @@ import { createClient } from "~/utils/supabase/server";
 
 export default async function DashboardPage() {
   const supabase = createClient();
+  
 
   const { data, error } = await supabase.auth.getUser();
   if (error || !data?.user) {
     redirect("/login");
   }
-  return <h1>Dashboard</h1>;
+  return (
+    <main className="m-2 p-4">
+      <header className="h-40 w-full"><h1>Dashboard</h1></header>
+      <aside>navigation</aside>
+      <section>
+        <article>user profile</article>
+        {data.user.email}
+      </section>
+    </main>
+  );
 }
