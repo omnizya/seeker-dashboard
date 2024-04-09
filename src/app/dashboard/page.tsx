75d38b1833bfbbf4b644a07288d72a9a8f15ab06
moughamir <moughamir@gmail.com>
2024-04-09 02:08:01 +0000

    [+] Jummal

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,6 @@
 import { redirect } from "next/navigation";
+import JummalCard from "~/components/Jomal/Jummal";
 
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Divider,
-  Link,
-  Image,
-  Textarea,
-  User,
-  Button
-} from "@nextui-org/react";
 import { createClient } from "~/utils/supabase/server";
 
 export default async function DashboardPage() {
@@ -26,40 +15,11 @@ export default async function DashboardPage() {
       <header className="h-40 w-full">
         <h1>Dashboard</h1>
       </header>
-      <aside> 
+      <aside>
       </aside>
       <section>
-        <article className="border  relative rounded-sm p-2 shadow-sm drop-shadow-2 w-1/2">
-          <Card className="w-full p-4">
-            <CardHeader className="flex gap-3 p-2">
-              <Image
-                alt="nextui logo"
-                height={40}
-                radius="sm"
-                src="https://www.arabiccalligraphygenerator.com/image?name=%D8%A7%D9%84%D8%AC%D9%8F%D9%85%D9%91%D9%84&font=0"
-                width={40}
-              />
-              <div className="flex flex-col m-2 p-2">
-                <p className=" text-title-md">حساب الجُمَّل</p>
-              </div>
-            </CardHeader>
-            <Divider />
-            <CardBody>
-              <Textarea
-  
-                placeholder="المرجو إدخال النص"
-                className="w-full p-2 "
-              />
-            </CardBody>
-            <Divider />
-            <CardFooter className="mt-2">
-            <Button size="lg">
-            احسب
-      </Button>  
-            </CardFooter>
-          </Card>
-        </article>
-        
+
+        <JummalCard />
       </section>
     </section>
   );
---
+++ src/components/Jomal/Jummal.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Image,
+  Textarea,
+} from "@nextui-org/react";
+import { useState } from "react";
+import { CalcJomalT } from "~/types";
+import { CalcJomal } from "~/utils";
+
+export default function JummalCard() {
+  const [jomalValues, setJomalValues] = useState<CalcJomalT>({
+    east: {
+      base: 0,
+      reduced: 0,
+    },
+    west: {
+      base: 0,
+      reduced: 0,
+    },
+  });
+  const submitContact = async (event: any) => {
+    event.preventDefault();
+    setJomalValues(CalcJomal(event.target.name.value));
+  };
+  return (
+    <article className="border  relative rounded-sm p-2 shadow-sm drop-shadow-2 w-1/2">
+      <Card className="w-full p-4">
+        <CardHeader className="flex gap-3 p-2">
+          <Image
+            alt="nextui logo"
+            height={40}
+            radius="sm"
+            src="https://www.arabiccalligraphygenerator.com/image?name=%D8%A7%D9%84%D8%AC%D9%8F%D9%85%D9%91%D9%84&font=0"
+            width={40}
+          />
+          <div className="flex flex-col m-2 p-2">
+            <p className=" text-title-md">حساب الجُمَّل</p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <Textarea
+            placeholder="المرجو إدخال النص"
+            className="w-full p-2 "
+            onChange={submitContact}
+          />
+        </CardBody>
+        <Divider />
+        <CardFooter className="mt-2">
+          {/*       <Button size="lg">
+          احسب
+    </Button>   */}
+        </CardFooter>
+      </Card>
+      <div className=" min-w-full bg-zinc-400 text-black text-center">
+        <h2 className="w-full bg-zinc-700 text-zinc-50 text-title-xl">
+          الحصيلة
+        </h2>
+        <table className=" border-collapse border border-slate-500 table-auto w-full">
+          <thead className=" table-header-group">
+            <tr>
+              <td className="border border-slate-600">مشرقي</td>
+              <td className="border border-slate-600">مغربي</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border border-slate-700">
+                {jomalValues.east.base} | {jomalValues.east.reduced}
+              </td>
+              <td className="border border-slate-700">
+                {jomalValues.west.base} | {jomalValues.east.reduced}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </article>
+  );
+}
