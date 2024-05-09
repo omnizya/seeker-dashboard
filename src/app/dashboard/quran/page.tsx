bdc46e865f1aedebbf7bceff49e26a2b1e6eb0b2
moughamir <moughamir@gmail.com>
2024-05-09 21:56:19 +0100

    pffffff

--- src/app/dashboard/quran/page.tsx
+++ src/app/dashboard/quran/page.tsx
@@ -6,20 +6,20 @@ import { Badge, Divider } from "@chakra-ui/react";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-function AyahComponent({ inputData }: Partial<Ayah>) {
+function AyahComponent({ values }: any) {
   return (
     <li className=" min-h-8 bg-slate-600 p-4 m-2 text-right ">
-      <Link href="/quran/[id]" as={`/quran/${inputData?.id}`}>
+      <Link href="/quran/[id]" as={`/quran/${values?.id}`}>
         <Badge variant="outline" colorScheme="green">
-          {inputData?.id}
+          {values?.id}
         </Badge>
 
-        <span className=" mr-4 font-uthman text-4xl">{inputData?.ayah}</span>
-        <Badge colorScheme="green">{inputData?.grand_east}</Badge>
-        <Badge colorScheme="red">{inputData?.grand_west}</Badge>
-        <Badge>{inputData?.small_east}</Badge>
-        <Badge>{inputData?.small_west}</Badge>
-        <Badge colorScheme="yellow">{inputData?.nafsy}</Badge>
+        <span className=" mr-4 font-uthman text-4xl">{values?.ayah}</span>
+        <Badge colorScheme="green">{values?.grand_east}</Badge>
+        <Badge colorScheme="red">{values?.grand_west}</Badge>
+        <Badge>{values?.small_east}</Badge>
+        <Badge>{values?.small_west}</Badge>
+        <Badge colorScheme="yellow">{values?.nafsy}</Badge>
       </Link>
     </li>
   );
@@ -35,7 +35,7 @@ export default function Index() {
   return (
     <ul className="min-h-10 max-h-300 overflow-y-auto max-w-[100ch] text-warning  bg-black">
       {data.map((p: Ayah) => (
-        <AyahComponent key={p.id} inputData={p} />
+        <AyahComponent key={p.id} values={p} />
       ))}
     </ul>
   );
