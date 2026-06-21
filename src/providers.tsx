7ba20f0c0e6b2c0d72b6f16141a53b8abf28125f
Mohamed Moughamir <moughamir@gmail.com>
2026-06-21 17:38:27 +0100

    feat: wire up next-themes and import Tailwind globals

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,18 +1,29 @@
 "use client";
 
+import { ThemeProvider } from "next-themes";
+import { ChakraProvider } from "@chakra-ui/react";
+import { CacheProvider } from "@chakra-ui/next-js";
+import Fonts from "./styles/fonts";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./styles/theme";
-import Fonts from "./styles/fonts";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Fonts />
-      {children}
-      <Analytics framework="nextjs" disableAutoTrack={false} />
-      <SpeedInsights />
-    </ChakraProvider>
+    <CacheProvider>
+      <ChakraProvider theme={theme}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Fonts />
+          {children}
+          <Analytics framework="nextjs" disableAutoTrack={false} />
+          <SpeedInsights />
+        </ThemeProvider>
+      </ChakraProvider>
+    </CacheProvider>
   );
 }
