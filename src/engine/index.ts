e5a5c28304de9398f8ee709b7a21c9c12702d733
Mohamed Moughamir <moughamir@gmail.com>
2026-06-22 01:40:05 +0100

    feat: add engine barrel export

---
+++ src/engine/index.ts
@@ -0,0 +1,21 @@
+export { LodgeKernel } from "./kernel";
+export type { LodgeState, WafqResult } from "./kernel";
+export {
+  IQRA_SEED,
+  STATE,
+  PLANETS,
+  CHALDEAN_ORDER,
+  DAY_RULERS,
+  ARCHETYPES,
+  ELEMENTS,
+  LODGE_STATE_SIZE,
+  SLOT,
+} from "./constants";
+export type {
+  StateFlags,
+  PlanetFlags,
+  ArchetypeModifier,
+  ArchetypeName,
+  Element,
+  SlotIndex,
+} from "./constants";
